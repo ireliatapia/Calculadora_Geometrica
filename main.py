c4f8b26c2c69d99cbcae1c82f11ebeacfeb1d5cb
David2809-1 <balonmn48@gmail.com>
2026-06-18 16:12:43 -0600

    Estudiante B: Agrega funcion calculae_area_circulo

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+import math
+def calcular_area_circulo(radio):
+ return math.pi * (radio ** 2)
 print("--- BIENVENIDO A LA CALCULADORA GEOMÉTRICA ---")
+print(f"Área de un círculo de radio 3: {calcular_area_circulo(3):.2f}")
